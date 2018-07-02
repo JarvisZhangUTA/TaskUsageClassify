29eba5506d6f17d75bd5892da27a0faba2901501
jarvis <zhangjw.uta@gmail.com>
2018-07-02 15:06:28 +0000

    ids init

--- get_commands.py
+++ get_commands.py
@@ -9,7 +9,7 @@ cursor = connection.cursor()
 mod = 0
 table_name = 'job_ids_mod_' + str(mod)
 
-cursor.execute('select job_id, count(*) as count, avg(end_time - start_time) as avg, group_concat(end_time - start_time) as durations from %s' % (table_name))
+cursor.execute('select job_id, count(*) as count, avg(end_time - start_time) as avg, group_concat(end_time - start_time) as durations from %s group by job_id' % (table_name))
 
 for row in cursor:
   print row
