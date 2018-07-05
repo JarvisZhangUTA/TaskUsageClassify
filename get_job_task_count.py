18e9f1d814e504075716d5a9f20114bd3982765a
JarvisZhangUTA <zhangjw.uta@gmail.com>
2018-07-04 21:25:35 -0500

    init

--- get_job_task_count.py
+++ get_job_task_count.py
@@ -3,10 +3,10 @@ import pymysql.cursors
 connection = pymysql.connect(host='localhost', port=3306, user='root', db='cluster', cursorclass=pymysql.cursors.DictCursor)
 cursor = connection.cursor()
 
-# with open('job_id_task_count.txt', 'a') as out_file:
-# for mod in range(50):
-mod = 0
-cursor.execute(' SELECT job_id,count(*) from `job_ids_mod_' + str(mod) + '` group by job_id')
-for row in cursor:
-    print row
-            # out_file.write()
+with open('job_id_task_count.txt', 'a') as out_file:
+    for mod in range(50):
+        cursor.execute(' SELECT job_id,count(*) as count from `job_ids_mod_' + str(mod) + '` group by job_id')
+        for row in cursor:
+            out_file.write( str(row['job_id']) + ' ' + str(row['count']) + '\n' )
+    print mod
+out_file.close()
