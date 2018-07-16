3587af2f66921dcd59b4b807ed6a1cb74718b09e
huiyangli <huiyang.li92@gmail.com>
2018-07-16 02:04:42 -0500

    min1000000

--- sort.py
+++ sort.py
@@ -1,5 +1,5 @@
 
-f = open('/Users/mac/Code/TaskUsageClassify/100000.txt')
+f = open('/Users/mac/Code/TaskUsageClassify/1000000.txt')
 out_file = open('/Users/mac/Code/TaskUsageClassify/sort_test.txt', 'a')
 result= []
 iter_f=iter(f)
